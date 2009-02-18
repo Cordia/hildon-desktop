f27c5733b0089d37bade70777d9e31a3190e2445
apinheiro <apinheiro@7feff2eb-67fc-0310-aff0-fef983b50a43>
2009-02-18 16:26:36 +0000

    2009-02-18  Alejandro Pinheiro <apinheiro@igalia.com>
    	* src/a11y/launcher/hda-launcher-grid.c: simplified after CAIL changes
    
    
    
    git-svn-id: https://projects.maemo.org/svn/af/packages/fremantle/trunk/hildon-desktop@35570 7feff2eb-67fc-0310-aff0-fef983b50a43

--- src/a11y/launcher/hda-launcher-grid.c
+++ src/a11y/launcher/hda-launcher-grid.c
@@ -49,13 +49,6 @@ hda_launcher_grid_init                          (HdaLauncherGrid *root);
 static void
 hda_launcher_grid_initialize                    (AtkObject *obj,
                                                  gpointer data);
-static gint
-hda_launcher_grid_get_n_children                (AtkObject *obj);
-
-static AtkObject*
-hda_launcher_grid_ref_child                     (AtkObject *obj,
-                                                 gint i);
-
 static G_CONST_RETURN gchar *
 hda_launcher_grid_get_name                      (AtkObject *obj);
 
@@ -69,10 +62,8 @@ hda_launcher_grid_class_init                            (HdaLauncherGridClass *k
   AtkObjectClass *class = ATK_OBJECT_CLASS (klass);
 
   /* AtkObject */
-  class->get_n_children = hda_launcher_grid_get_n_children;
-  class->ref_child = hda_launcher_grid_ref_child;
   class->initialize = hda_launcher_grid_initialize;
-  class->get_name = hda_launcher_grid_get_name;
+  class->get_name   = hda_launcher_grid_get_name;
 }
 
 static void
@@ -85,7 +76,7 @@ hda_launcher_grid_init                          (HdaLauncherGrid *grid)
 AtkObject*
 hda_launcher_grid_new                           (ClutterActor *grid)
 {
-  GObject *object = NULL;
+  GObject   *object     = NULL;
   AtkObject *accessible = NULL;
 
   object = g_object_new (HDA_TYPE_LAUNCHER_GRID, NULL);
@@ -105,64 +96,6 @@ hda_launcher_grid_initialize                    (AtkObject   *obj,
   ATK_OBJECT_CLASS (hda_launcher_grid_parent_class)->initialize (obj, data);
 
   obj->role = ATK_ROLE_FILLER;
-}
-
-static gint
-hda_launcher_grid_get_n_children                (AtkObject *obj)
-{
-  GList *children = NULL;
-  ClutterActor *actor = NULL;
-  gint num = 0;
-
-  g_return_val_if_fail (HDA_IS_LAUNCHER_GRID (obj), 0);
-
-  actor = CLUTTER_ACTOR (atk_gobject_accessible_get_object (ATK_GOBJECT_ACCESSIBLE (obj)));
-  if (actor == NULL)
-    {
-      return 0;
-    }
-
-  g_return_val_if_fail (CLUTTER_IS_CONTAINER (actor), 0);
-
-  /* Probably this is not a really efficient method, but as HdLauncherGrid has
-     no public methods to get the children, this is the only way to do thay */
-  children = clutter_container_get_children (CLUTTER_CONTAINER (actor));
-  num = g_list_length (children);
-
-  g_list_free (children);
-
-  return num;
-}
-
-static AtkObject*
-hda_launcher_grid_ref_child                     (AtkObject *obj,
-                                                 gint i)
-{
-  ClutterActor *grid = NULL;
-  ClutterActor *actor = NULL;
-  GList *children = NULL;
-  AtkObject *result = NULL;
-
-  g_return_val_if_fail (HDA_IS_LAUNCHER_GRID (obj), NULL);
-
-  grid = CLUTTER_ACTOR (atk_gobject_accessible_get_object (ATK_GOBJECT_ACCESSIBLE (obj)));
-
-  if (grid == NULL) /* State is defunct */
-    {
-      return NULL;
-    }
-
-  g_return_val_if_fail (HD_IS_LAUNCHER_GRID (grid), NULL);
-  g_return_val_if_fail (CLUTTER_IS_CONTAINER (grid), NULL);
-
-  children = clutter_container_get_children (CLUTTER_CONTAINER (grid));
-  actor = g_list_nth_data (children, i);
-
-  result = atk_gobject_accessible_for_object (G_OBJECT (actor));
-
-  g_object_ref (result);
-
-  return result;
 }
 
 static G_CONST_RETURN gchar *
