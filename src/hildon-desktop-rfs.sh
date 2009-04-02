ffe8d732d13f32aa786298dc32cf6a416e2d9002
schulhof <schulhof@7feff2eb-67fc-0310-aff0-fef983b50a43>
2009-04-02 07:04:25 +0000

    Retain applets across RFS
    git-svn-id: https://projects.maemo.org/svn/af/packages/fremantle/trunk/hildon-desktop@37536 7feff2eb-67fc-0310-aff0-fef983b50a43

--- src/hildon-desktop-rfs.sh
+++ src/hildon-desktop-rfs.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-rm -rf $HOME/.config/hildon-desktop
+
+CFGDIR=$HOME/.config/hildon-desktop
+
+for file in $(ls -A $CFGDIR); do
+  if ! echo $file | grep -q home.plugins; then
+    rm -rf $file
+  fi
+done
+
+if ! rmdir $CFGDIR; then
+  echo -n '' # NOOP
+fi
