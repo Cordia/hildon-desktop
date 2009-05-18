338eaffd8a4bd56df803b07bc1562e42d321ba50
jpetersen <jpetersen@7feff2eb-67fc-0310-aff0-fef983b50a43>
2009-05-18 07:52:19 +0000

    2009-05-18  Jan Arne Petersen  <jpetersen@openismus.com>
    	Fixes: NB#103823 - Unable to restore home applets from backup and
    	restore application.
    
    	* src/Makefile.am:
    	* src/hildon-desktop.prebackup.sh (added): Add prebackup script which
    	copies the system plugins config to user config if that not exist yet.
    
    
    
    git-svn-id: https://projects.maemo.org/svn/af/packages/fremantle/trunk/hildon-desktop@39099 7feff2eb-67fc-0310-aff0-fef983b50a43

---
+++ src/hildon-desktop.prebackup.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+HILDON_DESKTOP_DIR=$HOME/.config/hildon-desktop
+HOME_USER_PLUGINS=$HILDON_DESKTOP_DIR/home.plugins
+HOME_SYSTEM_PLUGINS=/etc/hildon-desktop/home.plugins
+
+if [ ! -d "$HILDON_DESKTOP_DIR" ] ; then
+  mkdir -p "$HILDON_DESKTOP_DIR";
+fi
+
+if [ ! -f "$HOME_USER_PLUGINS" ] ; then
+  cp "$HOME_SYSTEM_PLUGINS" "$HOME_USER_PLUGINS"
+fi
+
