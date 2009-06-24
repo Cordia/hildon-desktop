7977e4d2aa6754d3352acd89b495269c0a292d3e
gw <gw@7feff2eb-67fc-0310-aff0-fef983b50a43>
2009-06-24 14:44:53 +0000

    Remove overshoot until I've fixed it
    git-svn-id: https://projects.maemo.org/svn/af/packages/fremantle/trunk/hildon-desktop@40455 7feff2eb-67fc-0310-aff0-fef983b50a43

--- src/home/hd-home-view-container.c
+++ src/home/hd-home-view-container.c
@@ -768,13 +768,14 @@ hd_home_view_container_scroll_back (HdHomeViewContainer *container, gint velocit
   /* if no velocity, make something up */
   if (velocity == 0)
     {
+      priv->animation_overshoot = FALSE;
       velocity = width*5;
     }
   else
     {
       /* Overshoot if we were going in one direction, but expect to
        * go in the other */
-      priv->animation_overshoot = (velocity>0) != (priv->offset>0);
+      priv->animation_overshoot = FALSE;//(velocity>0) != (priv->offset>0);
       /* make sure velocity is within a sensible range, we don't want this
        * taking more than a second, or totally flicking past... */
       velocity = ABS(velocity);
