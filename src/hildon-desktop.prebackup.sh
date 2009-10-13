26044e395a173a027db0af6bb30a0e63fcd3122d
jpetersen <jpetersen@7feff2eb-67fc-0310-aff0-fef983b50a43>
2009-10-13 16:29:44 +0000

    Make sure home.plugins is restored after GConf values are restored. 	Fixes: NB#129810 - Home shortcuts backedup in one home view are 	restored in another view in a scenario 	* src/hildon-desktop-restore.sh: Restore home.plugins-backup to 	home.plugins 	* src/hildon-desktop.conf: Backup home.plugins-backup 	* src/hildon-desktop.prebackup.sh: Copy home.plugins to 	home.plugins-backup. 	* src/Makefile.am: Add new restore script.
    git-svn-id: https://projects.maemo.org/svn/af/packages/fremantle/trunk/hildon-desktop@42692 7feff2eb-67fc-0310-aff0-fef983b50a43

--- src/hildon-desktop.prebackup.sh
+++ src/hildon-desktop.prebackup.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 HILDON_DESKTOP_DIR=$HOME/.config/hildon-desktop
 HOME_USER_PLUGINS=$HILDON_DESKTOP_DIR/home.plugins
+HOME_USER_PLUGINS_BACKUP=$HOME_USER_PLUGINS-backup
 HOME_SYSTEM_PLUGINS=/etc/hildon-desktop/home.plugins
 
 if [ ! -d "$HILDON_DESKTOP_DIR" ] ; then
@@ -11,3 +12,4 @@ if [ ! -f "$HOME_USER_PLUGINS" ] ; then
   cp "$HOME_SYSTEM_PLUGINS" "$HOME_USER_PLUGINS"
 fi
 
+cp -f "$HOME_USER_PLUGINS" "$HOME_USER_PLUGINS_BACKUP"
