5833113c6a963dc6dcb4b359ce7c83d1da79be91
jpetersen <jpetersen@7feff2eb-67fc-0310-aff0-fef983b50a43>
2009-10-16 09:50:21 +0000

    Fix failed commit.
    git-svn-id: https://projects.maemo.org/svn/af/packages/fremantle/trunk/hildon-desktop@42778 7feff2eb-67fc-0310-aff0-fef983b50a43

--- src/home/hd-render-manager.c
+++ src/home/hd-render-manager.c
@@ -781,9 +781,9 @@ hd_render_manager_set_input_viewport()
        * because we might be right after a place_titlebar_elements()
        * which could just have moved it. */
       if ((STATE_IS_PORTRAIT (priv->state) && priv->status_area
--           && CLUTTER_ACTOR_IS_VISIBLE (priv->status_area))
--         /* also in the case of "dialog blur": */
--         || (STATE_ONE_OF(priv->state, HDRM_STATE_APP|HDRM_STATE_APP_PORTRAIT)
+           && CLUTTER_ACTOR_IS_VISIBLE (priv->status_area))
+          /* also in the case of "dialog blur": */
+          || (STATE_ONE_OF(priv->state, HDRM_STATE_APP|HDRM_STATE_APP_PORTRAIT)
               && priv->status_area
               && hd_render_manager_actor_is_visible(priv->status_area)
               /* FIXME: the following check does not work when there are
