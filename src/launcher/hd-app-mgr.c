437996f7b9778068f5c9101c7a0136118a5a3860
Moises Martinez <moimart@gmail.com>
2009-11-29 17:06:43 +0200

    Commented out MCE usage.
    All MCE related functions are #ifdef HAVE_DSME.
    This is TODO to replace MCE related functions with DSME ones.
    (cherry picked from commit 1143292c633d424ca2bc73a15245f4c5f92610e5)

--- src/launcher/hd-app-mgr.c
+++ src/launcher/hd-app-mgr.c
@@ -42,8 +42,11 @@
 #include <dbus/dbus-glib-bindings.h>
 #include <dbus/dbus-glib-lowlevel.h>
 #include <gconf/gconf-client.h>
+#ifdef HAVE_DSME
+/* TODO Convert MCE to DSME */
 #include <mce/dbus-names.h>
 #include <mce/mode-names.h>
+#endif
 #include "hd-launcher.h"
 #include "hd-launcher-tree.h"
 #include "home/hd-render-manager.h"
@@ -1896,6 +1899,8 @@ hd_app_mgr_dbus_signal_handler (DBusConnection *conn,
                                    INIT_DONE_SIGNAL_INTERFACE,
                                    INIT_DONE_SIGNAL_NAME))
     priv->init_done = TRUE;
+#ifdef HAVE_DSME
+/* TODO Convert MCE to DSME */
   else
     {
       changed = FALSE;
@@ -1916,6 +1921,7 @@ hd_app_mgr_dbus_signal_handler (DBusConnection *conn,
           hd_app_mgr_update_portraitness(self);
         }
     }
+#endif
 
   if (changed)
     hd_app_mgr_state_check ();
@@ -1948,6 +1954,8 @@ hd_app_mgr_mce_activate_accel_if_needed (gboolean update_portraitness)
       return;
     }
 
+#ifdef HAVE_DSME
+/* TODO Convert MCE to DSME */
   /* We're only interested in these signals if we're going to rotate. */
   if (activate)
     {
@@ -2005,6 +2013,7 @@ hd_app_mgr_mce_activate_accel_if_needed (gboolean update_portraitness)
 
   g_debug ("%s: %s", __FUNCTION__, activate ? "enabled" : "disabled");
   priv->accel_enabled = activate;
+#endif
 
   if (update_portraitness)
     hd_app_mgr_update_portraitness(the_app_mgr);
