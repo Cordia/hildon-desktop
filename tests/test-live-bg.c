dda834d343020b76a5611a471f05d559f5b105d1
Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
2010-02-02 11:06:10 +0200

    fix test-live-bg.c to make it use X11 only

--- tests/test-live-bg.c
+++ tests/test-live-bg.c
@@ -1,29 +1,13 @@
-#include <gtk/gtk.h>
-#include <gdk/gdkscreen.h>
-#include <cairo.h>
-#include <math.h>
+/* Simple test program that does not depend on Gtk/Gdk, to show how
+ * fullscreen etc. properties can be set. */
 
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
-#include <gtk/gtk.h>
-#include <gdk/gdkx.h>
-#include <gdk/gdkwindow.h>
+#include <X11/Xutil.h>
 #include <string.h>
+#include <stdio.h>
 
-/* This just attempts to paint itself at FPS fps and outputs the actual fps it
-   has managed. You can then run 'xresponse -i' to check 
-   how fast hildon-desktop is rendering it, or top to see CPU usage. */
-
-#define FPS 1
-
-/* #undef this to make the test run in non-composited mode */
-#define COMPOSITED 1
-
-/* Area's width and height to render */
-#define AREAW 800
-#define AREAH 480
-
-#ifndef COMPOSITED
+/*
 static void set_non_compositing (Display *display, Window xwindow)
 {
         Atom atom;
@@ -37,15 +21,27 @@ static void set_non_compositing (Display *display, Window xwindow)
                        XA_INTEGER, 32, PropModeReplace,
                        (unsigned char *) &one, 1);
 }
-#endif
+*/
+
+static void set_fullscreen (Display *dpy, Window w)
+{
+  Atom wm_state, state_fs;
+
+  wm_state = XInternAtom (dpy, "_NET_WM_STATE", False);
+  state_fs = XInternAtom (dpy, "_NET_WM_STATE_FULLSCREEN", False);
+
+  XChangeProperty (dpy, w, wm_state,
+                   XA_ATOM, 32, PropModeReplace,
+                   (unsigned char *) &state_fs, 1);
+}
 
 static void set_live_bg (Display *display, Window xwindow)
 {
         Atom atom;
         int one = 1;
 
-        g_printerr("%s: xwin %x\n", __func__, (unsigned) xwindow);
         atom = XInternAtom (display, "_HILDON_LIVE_DESKTOP_BACKGROUND", False);
+        fprintf (stderr, "XID: 0x%x\n", (unsigned)xwindow);
 
         XChangeProperty (display,
                        xwindow,
@@ -54,89 +50,124 @@ static void set_live_bg (Display *display, Window xwindow)
                        (unsigned char *) &one, 1);
 }
 
-/* This is called when we need to draw the windows contents */
-static gboolean expose(GtkWidget *widget, GdkEventExpose *event, gpointer userdata)
+static void draw_rect (Display *dpy, Window w, GC gc, XColor *col,
+                       int x, int y)
 {
-    cairo_t *cr = gdk_cairo_create(widget->window);
-    static GTimer *timer = 0;
-    static int frame = 0;
-    static double last_time = 0;
-    double this_time;
-    int width, height;
-    
-    if (!timer) timer = g_timer_new();
-    this_time = g_timer_elapsed (timer, NULL);
-
-    cairo_set_source_rgb (cr, 1.0, 1.0, 1.0); /* opaque white */
-
-    /* draw the background */
-    cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
-    //cairo_paint (cr);
-
-    
-    gtk_window_get_size(GTK_WINDOW(widget), &width, &height);
-
-    cairo_set_source_rgb(cr, 1, (sin(this_time)+1.0)/2.0, 0.2);
-    cairo_rectangle(cr, 0, 0, AREAW, AREAH);
-    cairo_fill(cr);
-    cairo_stroke(cr);
-
-    cairo_destroy(cr);
-
-    if (event->count == 0)
-      {
-        frame++;
-        if (frame > 100) 
-          {	    
-	    double ms = ((this_time - last_time)*1000) / frame;
-	    printf("Redraw time: %f ms per frame - %f fps\n", ms, 1000/ms);
-	    last_time = this_time;
-	    frame = 0;
-	  }
-      }
-    return FALSE;
+  XSetForeground (dpy, gc, col->pixel);
+  XDrawRectangle (dpy, w, gc, x, y, 10, 10);
 }
 
-static gboolean timeout(GtkWidget *widget) {
-  gtk_widget_queue_draw_area(widget, 0, 0, AREAW, AREAH);
-  return TRUE;
+/*
+static void set_no_transitions (Display *dpy, Window w)
+{
+  Atom no_trans;
+  int one = 1;
+
+  no_trans = XInternAtom (dpy, "_HILDON_WM_ACTION_NO_TRANSITIONS", False);
+
+  XChangeProperty (dpy, w, no_trans,
+                   XA_CARDINAL, 32, PropModeReplace,
+                   (unsigned char *)&one, 1);
 }
+*/
+
+static void set_window_type (Display *dpy, Window w)
+{
+  Atom w_type, normal;
+
+  w_type = XInternAtom (dpy, "_NET_WM_WINDOW_TYPE", False);
+  normal = XInternAtom (dpy, "_NET_WM_WINDOW_TYPE_NORMAL", False);
+
+  XChangeProperty (dpy, w, w_type,
+                   XA_ATOM, 32, PropModeReplace,
+                   (unsigned char *) &normal, 1);
+}
+
+/* this can be used to set fullscreenness after mapping the window */
+#if 0
+static void set_fullscreen (Display *display, Window xwindow)
+{
+        XClientMessageEvent xclient;
+  
+#define _NET_WM_STATE_REMOVE        0    /* remove/unset property */
+#define _NET_WM_STATE_ADD           1    /* add/set property */
+#define _NET_WM_STATE_TOGGLE        2    /* toggle property  */  
+  
+  memset (&xclient, 0, sizeof (xclient));
+  xclient.type = ClientMessage;
+  xclient.window = xwindow;
+  xclient.message_type = XInternAtom (display, "_NET_WM_STATE", False);
+  xclient.format = 32;
+  xclient.data.l[0] = _NET_WM_STATE_ADD;
+  xclient.data.l[1] = XInternAtom (display, "_NET_WM_STATE_FULLSCREEN", False);
+  xclient.data.l[2] = None;
+  xclient.data.l[3] = 0;
+  xclient.data.l[4] = 0;
+  
+  XSendEvent (display, DefaultRootWindow (display), False,
+              SubstructureRedirectMask | SubstructureNotifyMask,
+              (XEvent *)&xclient);
+}
+#endif
 
 int main(int argc, char **argv)
 {
-    gtk_init(&argc, &argv);
+        Display *dpy;
+        Window w;
+        GC green_gc;
+        XColor green_col;
+        Colormap colormap;
+        char green[] = "#00FF00";
 
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Live background test");
-    g_signal_connect(G_OBJECT(window), "delete-event", gtk_main_quit, NULL);
+        /* TODO: X error handling is missing */
 
-    gtk_widget_set_app_paintable(window, TRUE);
-    //gtk_widget_set_double_buffered(window, FALSE);
+        dpy = XOpenDisplay(NULL);
+        w = XCreateSimpleWindow(dpy, DefaultRootWindow (dpy), 0, 0,
+                                800, 480, 0, 0, 0);
 
-    g_signal_connect(G_OBJECT(window), "expose-event", G_CALLBACK(expose), NULL);
+        colormap = DefaultColormap (dpy, 0);
+        green_gc = XCreateGC (dpy, w, 0, 0);
+        XParseColor (dpy, colormap, green, &green_col);
+        XAllocColor (dpy, colormap, &green_col);
 
-    /* toggle title bar on click - we add the mask to tell X we are interested
-     * in this event */
-    //gtk_window_set_decorated(GTK_WINDOW(window), FALSE);
+        set_fullscreen(dpy, w);
+        set_window_type(dpy, w);
+        set_live_bg (dpy, w);
 
-    g_timeout_add(1000/FPS, (GSourceFunc)timeout, window);
-
-    gtk_widget_realize (window);
-
-    Display *display = XOpenDisplay (NULL);
-
-#ifndef COMPOSITED
-    set_non_compositing (display, GDK_WINDOW_XID (GTK_WIDGET (window)->window));
+#if 0
+        /* tell the window manager that we want keyboard focus */
+        XWMHints *wmhints;
+        wmhints = XAllocWMHints();
+        wmhints->input = True;
+        wmhints->flags = InputHint;
+        XSetWMHints(dpy, w, wmhints);
+        XFree(wmhints);
 #endif
 
-    gtk_window_fullscreen (GTK_WINDOW (window));
+        /* optional: disable compositing for this window to speed up */
+        //set_non_compositing(dpy, w);
 
-    gtk_widget_show_all(window);
-    set_live_bg (display, GDK_WINDOW_XID (GTK_WIDGET (window)->window));
+        /* optional: disable compositor's transitions for this window */
+        //set_no_transitions(dpy, w);
 
-    gtk_main();
+        XSelectInput (dpy, w,
+                      ExposureMask | ButtonReleaseMask | ButtonPressMask);
+        XMapWindow(dpy, w);  /* map the window */
 
-    return 0;
+        for (;;) {
+                XEvent xev;
+
+                XNextEvent(dpy, &xev);
+
+                if (xev.type == Expose)
+                  draw_rect (dpy, w, green_gc, &green_col, 100, 100);
+                else if (xev.type == ButtonRelease) {
+                  XButtonEvent *e = (XButtonEvent*)&xev;
+                  draw_rect (dpy, w, green_gc, &green_col, e->x, e->y);
+                }
+        }
+
+        return 0;
 }
 
 
