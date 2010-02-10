3677ed298fb063e7aac06237281d68c715376e3d
Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
2010-02-10 17:55:37 +0200

    Optimise few clutter_group_get_n_children calls away, it's O(n).
      * src/home/hd-render-manager.c (hd_render_manager_restack): No need to
        call clutter_group_get_n_children in the first for loop.

--- src/home/hd-render-manager.c
+++ src/home/hd-render-manager.c
@@ -1848,11 +1848,13 @@ void hd_render_manager_restack()
   gint i, n_elements;
   GList *previous_home_blur = 0;
   unsigned int screenw, screenh;
+  int n_children;
 
   wm = MB_WM_COMP_MGR(priv->comp_mgr)->wm;
   /* Add all actors currently in the home_blur group */
 
-  for (i=0;i<clutter_group_get_n_children(CLUTTER_GROUP(priv->home_blur));i++)
+  n_children = clutter_group_get_n_children(CLUTTER_GROUP(priv->home_blur));
+  for (i = 0; i < n_children; ++i)
     {
       ClutterActor *child =
         clutter_group_get_nth_child(CLUTTER_GROUP(priv->home_blur), i);
