1803833d963c2f2cb7750f74bdf5e8e3b34a69fe
Moises Martinez <moimart@gmail.com>
2010-02-15 17:39:47 +0200

    * Improved press effect on desktop (cherry picked from commit 329e68b5a35e7a209929f9a2de3ed65cb4d73f90)

--- src/home/hd-render-manager.h
+++ src/home/hd-render-manager.h
@@ -277,6 +277,8 @@ void hd_render_manager_pause_blur_animation(void);
 
 void hd_render_manager_press_effect (void);
 
+void hd_render_manager_end_press_effect (void);
+
 G_END_DECLS
 
 #endif /* __HD_RENDER_MANAGER_H__ */
