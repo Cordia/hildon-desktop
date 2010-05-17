Cordia/hildon-desktop

commit 76650bd0a66e28d53b145816322bdcf883d7ba51

fix b0b56f55ee6b03fbb26e822455089d839666583a
we called our method on the wrong object :S  long live gobject

src/util/hd-dbus.c
@@ -158,9 +158,11 @@ hd_dbus_system_bus_signal_handler (DBusConnection *conn,
                   /* Stop redraws from anything. We do this on the stage
                    * because Rotation does it on HDRM, and we don't want to
                    * conflict. */
+#if 1
                   clutter_actor_hide(
                       CLUTTER_ACTOR(hd_render_manager_get()));
                   clutter_actor_set_allow_redraw(stage, FALSE);
+#endif
                   hd_dbus_display_is_off = TRUE;
                   /* Hiding before set_allow_redraw will queue a redraw,
                    * which will draw a black screen (because hdrm is hidden).

src/home/hd-render-manager.c
@@ -2341,6 +2341,12 @@ void hd_render_manager_update_blur_state()
           blur && has_video_overlay);
 }
 
+void hd_render_manager_pause_blur_animation()
+{
+  tidy_blur_group_stop_progressing(
+                    CLUTTER_ACTOR(the_render_manager->priv->home_blur));
+}
+
 /* This is called when we are in the launcher subview so that we can blur and
  * darken the background even more */
 void hd_render_manager_set_launcher_subview(gboolean subview)

src/home/hd-render-manager.h
@@ -269,6 +269,7 @@ gboolean hd_render_manager_actor_is_visible(ClutterActor *actor);
 void hd_render_manager_set_visibilities(void);
 
 void hd_render_manager_update_blur_state(void);
+void hd_render_manager_pause_blur_animation(void);
 
 G_END_DECLS
 

src/tidy/tidy-blur-group.h
@@ -50,8 +50,6 @@ void tidy_blur_group_set_source_changed(ClutterActor *blur_group);
 void tidy_blur_group_hint_source_changed(ClutterActor *blur_group);
 void tidy_blur_group_stop_progressing(ClutterActor *blur_group);
 
-
 G_END_DECLS
-
 
 #endif /*TIDYBLUR_H_*/

src/util/hd-transition.c
@@ -43,7 +43,6 @@
 #include "hd-title-bar.h"
 #include "hd-clutter-cache.h"
 #include "tidy/tidy-sub-texture.h"
-#include "tidy/tidy-blur-group.h"
 
 #include "hd-app.h"
 #include "hd-volume-profile.h"
@@ -1504,8 +1503,7 @@ hd_transition_rotating_fsm(void)
         /* Force redraw for screenshot *now*, before windows have a
          * chance to change.  Tell the render manager not to progress
          * the animation, it will be reset anyway. */
-        tidy_blur_group_stop_progressing(
-                                CLUTTER_ACTOR(hd_render_manager_get()));
+        hd_render_manager_pause_blur_animation();
         clutter_redraw(CLUTTER_STAGE(clutter_stage_get_default()));
         /* Start rotate transition */
         hd_util_set_rotating_property(Orientation_change.wm, TRUE);

commit b72c3302af8702dd841af39ad6ba99b7a7ec8078

fix 76650bd0a66e28d53b145816322bdcf883d7ba51
accidentally commited something
looks like nowadays i just cannot not break a commit

src/util/hd-dbus.c
@@ -158,11 +158,9 @@ hd_dbus_system_bus_signal_handler (DBusConnection *conn,
                   /* Stop redraws from anything. We do this on the stage
                    * because Rotation does it on HDRM, and we don't want to
                    * conflict. */
-#if 1
                   clutter_actor_hide(
                       CLUTTER_ACTOR(hd_render_manager_get()));
                   clutter_actor_set_allow_redraw(stage, FALSE);
-#endif
                   hd_dbus_display_is_off = TRUE;
                   /* Hiding before set_allow_redraw will queue a redraw,
                    * which will draw a black screen (because hdrm is hidden).